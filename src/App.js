b938595cd9b9e8185d35fc964191a467143ca0aa
Ruben Melendez <ruben@MacBook-Pro.local>
2021-04-11 16:58:07 -0700

    env var tests

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          env1: {process.env.REACT_APP_TEST_ENV_VAL}
+          env2: {process.env.REACT_APP_TEST_VAR}
         </p>
         <a
           className="App-link"
