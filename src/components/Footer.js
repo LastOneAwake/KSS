b5baa022fdabdc4a06f0401834287ac1e09a63d7
Ruben Melendez <ruben@MacBook-Pro.lan1>
2021-07-10 16:43:40 -0700

    added files, footer, and policy pdfs

---
+++ src/components/Footer.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './footer.scss';
+import fbIcon from '../assets/smIcons/facebook.png';
+import instaIcon from '../assets/smIcons/instagram.png';
+import tikTokIcon from '../assets/smIcons/tikTok.png';
+import pinterestIcon from '../assets/smIcons/pinterest.png';
+
+export default function Footer({ currentView }) {
+    const onContact = currentView === 'Contact';
+
+    return (
+        <div id='footer'>
+
+            <div
+                id='footerLinks'
+                className={onContact ? 'onContact' : ''}
+            >
+                <a className='footerLink' href='google.com'>Privacy</a>
+                <a className='footerLink' href='google.com'>Terms of Use</a>
+            </div>
+            {!onContact &&
+                <div id='footerSMIcons'>
+                    <div
+                        tabIndex='0'
+                        className='smIconHolder'
+                        onClick={() => {
+                            window.open('https://www.facebook.com/kikissheshed', '_blank')
+                        }}
+                    >
+                        <img src={fbIcon} alt='facebook link' />
+                    </div>
+                    <div
+                        tabIndex='0'
+                        className='smIconHolder'
+                        onClick={() => {
+                            window.open('https://www.instagram.com/kikis.she.shed/', '_blank')
+                        }}
+                    >
+
+                        <img src={instaIcon} alt='Instagram link' />
+                    </div>
+                    <div
+                        tabIndex='0'
+                        className='smIconHolder'
+                        onClick={() => {
+                            window.open('https://www.tiktok.com/@kikis.she.shed', '_blank')
+                        }}
+                    >
+
+                        <img src={tikTokIcon} alt='TikTok link' />
+                    </div>
+                    <div
+                        tabIndex='0'
+                        className='smIconHolder'
+                        onClick={() => {
+                            window.open('https://www.pinterest.com/kikissheshed/_created/', '_blank')
+                        }}
+                    >
+                        <img src={pinterestIcon} alt='Pinterest link' />
+                    </div>
+                </div>
+            }
+        </div >
+    )
+}
